5679b6dc0bbe422d54c3ad76f0442f4597ccb0c8
Felipe Gallego <114832482+FelipeLit@users.noreply.github.com>
2024-01-10 21:33:12 -0500

    Add files via upload

--- js/registro.js
+++ js/registro.js
@@ -1,72 +1,108 @@
-let table = document.querySelector('.table')
-let tbody = document.createElement('tbody');
+let table = document.querySelector("#table");
+let tbody = document.createElement("tbody");
 
+let result = fetch("https://memin.io/public/api/users")
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    data.forEach((user) => {
+      let row = document.createElement("tr");
+      table.appendChild(row);
 
-let result = fetch('https://memin.io/public/api/users')
-    .then(result =>{
-        return result.json()
+      let idCell = document.createElement("td");
+      idCell.innerHTML = user.id;
+      row.appendChild(idCell);
 
-    }).then(data =>{
-        data.forEach(user => {
-            
-            let row = document.createElement('tr')
-            table.appendChild(row)
+      let name = document.createElement("td");
+      name.innerHTML = user.name;
+      row.appendChild(name);
 
-            let idCell = document.createElement('td')
-            idCell.innerHTML = user.id
-            row.appendChild(idCell)
+      let email = document.createElement("td");
+      email.innerHTML = user.email;
+      row.appendChild(email);
 
-            let name = document.createElement('td')
-            name.innerHTML = user.name
-            row.appendChild(name)
+      let btnDelete = document.createElement("button");
+      btnDelete.classList.add("btn", "btn-danger", "mt-2", "me-3");
+      btnDelete.innerHTML = "Delate";
+      row.appendChild(btnDelete);
 
-            let email = document.createElement('td')
-            email.innerHTML = user.email
-            row.appendChild(email)
+      let btnUpdate = document.createElement("button");
+      btnUpdate.classList.add("btn", "btn-success", "mt-2", "me-3");
+      btnUpdate.innerHTML = "Update";
+      row.appendChild(btnUpdate);
 
-            let btnDelete = document.createElement('button')
-            btnDelete.classList.add('btn', 'btn-danger', 'mt-2', 'me-3')
-            btnDelete.innerHTML = 'Delate'
-            row.appendChild(btnDelete)
-
-            let btnUpdate = document.createElement('button')
-            btnUpdate.classList.add('btn', 'btn-success', 'mt-2', 'me-3')
-            btnUpdate.innerHTML = 'Update'
-            row.appendChild(btnUpdate)
-
-            let btnDetails = document.createElement('button')
-            btnDetails.classList.add('btn', 'btn-secondary', 'mt-2', 'me-3')    
-            btnDetails.innerHTML = 'Details'
-            row.appendChild(btnDetails)
-
-
-        });
-        
-        table.appendChild(tbody)
+      let btnDetails = document.createElement("button");
+      btnDetails.classList.add("btn", "btn-secondary", "mt-2", "me-3");
+      btnDetails.setAttribute('id', 'btn-details');
+      btnDetails.setAttribute('onclick', `detailsUser(${user.id})`);
+      btnDetails.innerHTML = "Details";
+      row.appendChild(btnDetails);
     });
 
-    let btnAddorUpdate = document.getElementById('btnAddorUpdate')
+    table.appendChild(tbody);
 
-    function updateUsers (){
+    //btnDetails.onclick = detailsUser;  
+  });
 
-        let nameUser = document.formUsers.nameUser.value
-        let emailUser = document.formUsers.emailUser.value
-        let passwordUser = document.formUsers.passwordUser.value
+//add new users
+let btnAddorUpdate = document.getElementById("btnAddorUpdate");
 
-        //console.log(nameUser, emailUser, passwordUser);
+function addUsers() {
+  let nameUser = document.formUsers.nameUser.value;
+  let emailUser = document.formUsers.emailUser.value;
+  let passwordUser = document.formUsers.passwordUser.value;
 
-        let url = fetch('https://memin.io/public/api/users');
+  //console.log(nameUser, emailUser, passwordUser);
 
-        fetch(url, {
-            method: "POST",
-            
-        })
+  let dates = {
+    name: nameUser,
+    email: emailUser,
+    password: passwordUser,
+  };
+
+  let url = "https://memin.io/public/api/users";
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(dates),
+    headers: { "content-type": "application/json" },
+  })
+  .then((response) => response.json());
+}
+
+//modal add user
+let btnModalAddUser = document.getElementById("myBtn");
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+
+btnModalAddUser.onclick = function () {
+  modal.style.display = "block";
+};
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//clear form of add user
+
+btnAddorUpdate.onclick = addUsers;
 
 
+//details user
+let btnDetails = document.getElementById("btn-details")
 
+function detailsUser(user) {
+   fetch(`https://memin.io/public/api/users/${user.id}`,{
+        method: "GET",
+        headers: {"Content-type" : "application/json"}
+    })
+    .then(response => response.json())
+}
 
-
-
-    }
-
-    btnAddorUpdate.onclick = updateUsers
+  
