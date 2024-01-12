ed1e5cc3b4f0e5d96a2b8793f2cceb8439bd4e7c
Felipe Gallego <114832482+FelipeLit@users.noreply.github.com>
2024-01-11 21:35:59 -0500

    Add files via upload

--- js/registro.js
+++ js/registro.js
@@ -24,25 +24,29 @@ let result = fetch("https://memin.io/public/api/users")
 
       let btnDelete = document.createElement("button");
       btnDelete.classList.add("btn", "btn-danger", "mt-2", "me-3");
-      btnDelete.innerHTML = "Delate";
+      btnDelete.innerHTML = "Delete";
+      btnDelete.setAttribute("id", "btn-delete");
+      btnDelete.setAttribute("onclick", `deleteUser(${user.id})`);
       row.appendChild(btnDelete);
 
       let btnUpdate = document.createElement("button");
       btnUpdate.classList.add("btn", "btn-success", "mt-2", "me-3");
+      btnUpdate.setAttribute("id", "btn-update");
+      btnUpdate.setAttribute("onclick", `bringDataUser(${user.id})`);
       btnUpdate.innerHTML = "Update";
       row.appendChild(btnUpdate);
 
       let btnDetails = document.createElement("button");
       btnDetails.classList.add("btn", "btn-secondary", "mt-2", "me-3");
-      btnDetails.setAttribute('id', 'btn-details');
-      btnDetails.setAttribute('onclick', `openDetails(${user.id})`);
+      btnDetails.setAttribute("id", "btn-details");
+      btnDetails.setAttribute("onclick", `openDetails(${user.id})`);
       btnDetails.innerHTML = "Details";
       row.appendChild(btnDetails);
     });
 
     table.appendChild(tbody);
 
-    //btnDetails.onclick = detailsUser;  
+    //btnDetails.onclick = detailsUser;
   });
 
 //add new users
@@ -65,9 +69,8 @@ function addUsers() {
   fetch(url, {
     method: "POST",
     body: JSON.stringify(dates),
-    headers: {"content-type": "application/json" },
-  })
-  .then((response) => response.json());
+    headers: { "content-type": "application/json" },
+  }).then((response) => response.json());
 }
 
 //modal add user
@@ -90,51 +93,112 @@ window.onclick = function (event) {
 };
 
 //clear form of add user
+//clean's code
 
 btnAddorUpdate.onclick = addUsers;
 
-
 //details user
 let btnDetails = document.getElementById("btn-details");
-let modalDetails = document.getElementById ('myModalDetails');
+let modalDetails = document.getElementById("myModalDetails");
 
-function openDetails(userid){
-  modalDetails.style.display = 'block'
-  
-  fetch(`https://memin.io/public/api/users/${userid}`,{
+function openDetails(userid) {
+  modalDetails.style.display = "block";
+
+  fetch(`https://memin.io/public/api/users/${userid}`, {
     method: "GET",
-    headers: {"Content-type" : "application/json"}
+    headers: { "Content-type": "application/json" },
   })
-    .then(response => response.json())
-    .then(details => {
-      let idUser = document.getElementById('idUser').innerHTML = details.id;
-      console.log(idUser);
-      
+    .then((response) => response.json())
+    .then((details) => {
+      let idUser = (document.getElementById("idUser").innerHTML = details.id);
+      let nameUser = (document.getElementById("nameUser").innerHTML =
+        details.name);
+      let emailUser = (document.getElementById("emailUser").innerHTML =
+        details.email);
+      let emailVerified = (document.getElementById("emailverified").innerHTML =
+        details.email_verified_at);
+      let passwordUser = (document.getElementById("password").innerHTML =
+        details.password);
+      let rememberToken = (document.getElementById("remembertoken").innerHTML =
+        details.remember_token);
+      let created = (document.getElementById("created").innerHTML =
+        details.created_at);
+      let Update = (document.getElementById("update").innerHTML =
+        details.created_at);
       //idUser.innerText = details.id;
-      
       //console.log(details);
-    
     });
 }
 
-
-/* 
-btnDetails.onclick = function () {
-  modalDetails.style.display = 'block'
-}
-
-span.onclick = function (){
-  modalDetails.style.display = 'none'
-}
-
-window.onclick = function (event){
-  if (event.target == modalDetails){
-    modalDetails.style.display = 'none'
+window.onclick = function (event) {
+  if (event.target == modalDetails) {
+    modalDetails.style.display = "none";
   }
-} */
+};
 
-window.onclick = function (event){
-  if (event.target == modalDetails){
-    modalDetails.style.display = 'none'
+//delete user
+
+let btnDeleteUser = document.getElementById("btn-delete");
+
+function deleteUser(iduser) {
+  if (confirm(`¿Are you sure you want to delete? ${iduser}`)) {
+    fetch(`https://memin.io/public/api/users/${iduser}`, {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => response.json()(window.location.reload()));
   }
 }
+
+//update user
+//bring information of user
+let nameUS = document.getElementById("nameForm");
+let emailUS = document.getElementById("emailForm");
+let passwordUS = document.getElementById("passwordForm");
+
+function bringDataUser(iduser) {
+  modal.style.display = "block";
+  
+  fetch(`https://memin.io/public/api/users/${iduser}`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      nameUS.value = data.name;
+      emailUS.value = data.email;
+      passwordUS.value = data.password;
+    });
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+//update information user
+let btnUpdate = document.getElementById("btn-update");
+
+function updateDataUSer(iduser){
+
+  let data = {
+    name: nameUS.value,
+    email: emailUS.value,
+    password: passwordUS.value,
+  };
+
+  fetch(`https://memin.io/public/api/users/${iduser}`, {
+
+  method: "PUT",
+  body: JSON.stringify(data),
+  headers: {'Content-Type': 'application/json'}
+  })
+  .then (response => console.log(response))
+  .then ((idUSer)=>{
+    id
+  })
+}
+
+btnUpdate.onclick = updateDataUSer()
+
